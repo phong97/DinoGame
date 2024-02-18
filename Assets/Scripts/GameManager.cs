6a38e3ae19d3e825a5ad32c6fb2c69966d6c69e9
phonghv <phonghv@vng.com.vn>
2024-02-18 17:51:22 +0700

    done game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +9,16 @@ public class GameManager : MonoBehaviour
     public float initialGameSpeed = 5f;
     public float gameSpeedInscrease = 0.1f;
     public float gameSpeed { get; private set; }
+
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiscoreText;
+    public Button retryButton;
+
+    private Player player;
+    private Spawner spawner;
+
+    private float score;
 
     private void Awake()
     {
@@ -32,16 +42,60 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+
         NewGame();
     }
 
-    private void NewGame()
+    public void NewGame()
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
+        score = 0f;
         gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+
+        UpdateHiscore();
     }
 
     private void Update()
     {
         gameSpeed += gameSpeedInscrease * Time.deltaTime;
+        score += gameSpeed * Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt(score).ToString("D5");
+    }
+
+    private void UpdateHiscore()
+    {
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0f);
+
+        if (score > hiscore) {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore", hiscore);
+        }
+
+        hiscoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,39 @@ public class Player : MonoBehaviour
     public float gravity = 9.81f * 2f;
     public float jumpForce = 8f;
 
-    private void Awake() {
+    private void Awake()
+    {
         character = GetComponent<CharacterController>();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         direction = Vector3.zero;
     }
 
-    private void Update() {
+    private void Update()
+    {
         direction += Vector3.down * gravity * Time.deltaTime;
 
-        if (character.isGrounded) {
+        if (character.isGrounded)
+        {
             direction = Vector3.down;
 
-            if (Input.GetButton("Jump")) {
+            if (Input.GetButton("Jump"))
+            {
                 direction = Vector3.up * jumpForce;
 
             }
         }
 
         character.Move(direction * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            GameManager.Instance.GameOver();
+        }
     }
 }
